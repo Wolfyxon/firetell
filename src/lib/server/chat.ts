d7ed271861737d6d1ada265a6ab61141745d0814
Wolfyxon <wolfyxon@gmail.com>
2026-03-03 09:15:12 +0100

    Chat creation logic

--- src/lib/server/chat.ts
+++ src/lib/server/chat.ts
@@ -9,25 +9,54 @@ export type MessageInit = {
     content: string
 }
 
+export type ChatInit = {
+    name?: string
+}
+
+export type Chat = ChatInit & {
+    createdAt: number
+}
+
 export type Message = MessageInit & {
     timestamp: number
-};
+}
 
 export async function sendMessage(chatId: string, msg: MessageInit): Promise<string> {
     getFrbAdmin();
     const db = getDatabase();
 
-    if(chatId.includes("/")) {
+    if(chatId.includes("/") || chatId.includes("..")) {
         throw new Error("Invalid chat ID");
     }
 
     const ref = db.ref(`chats/${chatId}/messages`);
 
-    const promise = ref.push({
+    const res = await ref.push({
         ...msg,
         timestamp: Date.now()
-    });
+    } as Message);
 
-    await promise;
-    return promise.key;
+    if(!res.key) {
+        throw new Error("Key null");
+    }
+
+    return await res.key;
+}
+
+export async function createChat(chatInit: ChatInit): Promise<string> {
+    getFrbAdmin();
+    const db = getDatabase();
+
+    const ref = db.ref(`chats`);
+
+    const res = await ref.push({
+        ...chatInit,
+        createdAt: Date.now()
+    } as Chat);
+
+    if(!res.key) {
+        throw new Error("Key null");
+    }
+
+    return res.key;
 }
