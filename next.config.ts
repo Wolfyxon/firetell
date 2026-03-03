fdb4a31a9b6184c9a8ecf081434364f97ab5c663
Wolfyxon <wolfyxon@gmail.com>
2026-03-03 07:44:21 +0100

    Alt env loading

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
+if(process.env.ENV_FILE) {
+  console.log("Using env file", process.env.ENV_FILE);
+  process.loadEnvFile(process.env.ENV_FILE);
+}
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
