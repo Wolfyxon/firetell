83886cb0de713e043b95463d7f18bea7beeb949b
Wolfyxon <wolfyxon@gmail.com>
2026-03-05 08:40:01 +0100

    Init sending ui logic

--- src/app/chat/ChatPanel/ChatPanel.tsx
+++ src/app/chat/ChatPanel/ChatPanel.tsx
@@ -1,8 +1,28 @@
 "use client"
 
+import { KeyboardEvent, useState } from "react";
 import "./style.css";
 
 export default function ChatPanel() {
+    const [msgInput, setMsgInput] = useState("");
+
+    function keydown(e: KeyboardEvent) {
+        if(e.key == "Enter" && !e.shiftKey) {
+            e.preventDefault();
+            send();
+        }
+    }
+
+    function send() {
+        const content = msgInput.trim();
+
+        if(content.length == 0) {
+            return;
+        }
+
+        setMsgInput("");
+    }
+
     return (
         <div id="chat-panel">
             <div id="chat-messages">
@@ -13,6 +33,9 @@ export default function ChatPanel() {
                     className="input input-dark"
                     id="inp-message"
                     placeholder="Type your message..."
+                    value={msgInput}
+                    onChange={(e) => setMsgInput(e.target.value)}
+                    onKeyDown={keydown}
                 />
             </div>
         </div>
